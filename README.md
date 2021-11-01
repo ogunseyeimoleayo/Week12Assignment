60b757b864d7d15a499c894e9ae20eace62aa832
unknown <ogunseyeimoleayo@gmail.com>
2021-11-01 21:33:47 +0100

    Updating comments and readme file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# queensMedicalCenter
+# QueensMedicalCenter
+
+### About the App
+This a simple web application showing user registration and login.
+It also shows how we can authentication user by making sure only users with invalid username and password are denied access
+It also shows user authorization by making sure that registered users have access to the resources on the web portal, in
+this case it is a "Successful Login" message.
+
+### Pages
+ * index.html - This is the entrypoint of the application, 
+                 it is the landing page which has links to the other pages
+ * login.html - This is the login page, it is called when the user clicks on the signing in link. 
+                It accepts username and password.
+ * register.html - This is the registration page, it is called when the signup link is clicked.
+ * user.html - This page is rendered when user is logged in successfully.
+
+### The Functions
+All backend functions are in app.py file
+
+### How to run
+* Install Python and import Flask library
+* Run python app.py to see the address of the application
+* Open http://127.0.0.1:5000/ on browser to access the application
+
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+# Using Flask, python's framework for web application
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Creating a global variable registered_user to keep the list of all users who registered
 registered_user = []
 
 
-class users:
+# Creating class user showing characteristics of the users that is going to be inputted
+class Users:
     def __init__(self, firstname, lastname, email, username, password):
         self.firstname = firstname
         self.lastname = lastname
@@ -14,29 +17,36 @@ class users:
         self.password = password
 
 
+# This method renders the landing page - index.html
 @app.route("/")
 def main():
     return render_template('index.html')
 
 
+# This method handles the rendering of the login page with GET and
+# also authenticates the user with the POST method.
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
+        # check the list of registered user for username and password
         for user in registered_user:
             if request.form['username'] == user.username and request.form['password'] == user.password:
                 return render_template('user.html')
         else:
             error = 'Invalid Credentials. Please try again.'
-
+    # renders the login page if method is GET or the credential is invalid
     return render_template('login.html', error=error)
 
 
+# This method handles the user's registration with POST
+# and renders the registration page with GET
 @app.route("/showSignUp", methods=['GET', 'POST'])
 def register():
     error = None
     if request.method == 'POST':
-        if request.form['password'] != request.form['confirmpassword']:
+        # checks if there is a match between the password and confirm_password
+        if request.form['password'] != request.form['confirm_password']:
             error = 'Password mismatch. Please try again.'
         else:
             firstname = request.form['firstname']
@@ -44,10 +54,12 @@ def register():
             email = request.form['email']
             username = request.form['username']
             password = request.form['password']
-            registered_user.append(users(firstname, lastname, email, username, password))
+            registered_user.append(Users(firstname, lastname, email, username, password))
             return render_template('index.html', message="Please log in with your new username and password")
+    # renders the register.html page if method is GET or if an error occurred
     return render_template('register.html', error=error)
 
 
+# This is the main method where execution starts
 if __name__ == "__main__":
     app.run()
